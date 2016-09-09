1962c1e0385ee67454a4e37e80ffa86f0bf1b49a
Shawnpoon <shawnpoon@qq.com>
2016-09-10 00:03:24 +0800

    new

---
+++ src/main/java/com/joosure/yjj/listener/InitialListener.java
@@ -0,0 +1,19 @@
+package com.joosure.yjj.listener;
+
+import com.shawn.server.core.listener.PreContextInitListener;
+
+public class InitialListener extends PreContextInitListener {
+
+	@Override
+	public void Initial() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void Destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
