09dd494197532cc085a2b145b977710cc1620841
Shawnpoon <shawnpoon@qq.com>
2016-09-14 02:23:15 +0800

    update

--- src/main/java/com/joosure/yjj/service/WebsiteServiceImpl.java
+++ src/main/java/com/joosure/yjj/service/WebsiteServiceImpl.java
@@ -1,14 +1,12 @@
 package com.joosure.yjj.service;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.joosure.service.api.WebsiteService;
 
-@Service
 public class WebsiteServiceImpl implements WebsiteService {
 
 	@Override
 	public String getAmountTotal() {
-		return null;
+		return "hello";
 	}
 
 }
