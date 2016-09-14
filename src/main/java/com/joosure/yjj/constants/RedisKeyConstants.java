61268b734ed8c0fdd82c7e044ed55920a7e17b92
Shawn Poon <shawnpoon@qq.com>
2016-09-14 16:17:49 +0800

    add so redis support

--- src/main/java/com/joosure/yjj/constants/RedisKeyConstants.java
+++ src/main/java/com/joosure/yjj/constants/RedisKeyConstants.java
@@ -2,6 +2,8 @@ package com.joosure.yjj.constants;
 
 public class RedisKeyConstants {
 
-	public static final String website_amount_total = "website_amount_total";
+	public static final String website_amount_total = "website_amount_total"; // 总额
+
+	public static final String website_data_list = "website_data_list"; // 轮播数据
 
 }
--- src/main/java/com/joosure/yjj/dao/redis/WebsiteRedisDao.java
+++ src/main/java/com/joosure/yjj/dao/redis/WebsiteRedisDao.java
@@ -1,5 +1,10 @@
 package com.joosure.yjj.dao.redis;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisCallback;
@@ -11,6 +16,8 @@ import com.shawn.server.core.redis.RedisGeneratorDao;
 
 @Repository(value = "websiteRedisDao")
 public class WebsiteRedisDao extends RedisGeneratorDao<String, String> {
+
+	private static final Logger LOG = LoggerFactory.getLogger("redis_log");
 
 	/**
 	 * 取得前端显示的总额，若为空，返回0；
@@ -34,6 +41,26 @@ public class WebsiteRedisDao extends RedisGeneratorDao<String, String> {
 		});
 
 		return amount;
+	}
+
+	/**
+	 * 通过偏移量获得轮播数据，每次获取100条<br>
+	 * 执行命令为：lrange 'website_data_list' 'offset' 'offset+100'
+	 * 
+	 * @param offset
+	 * @return
+	 */
+	public List<String> getRollListDataByOffset(final long offset) {
+		List<String> list = null;
+		try {
+			list = redisTemplate.opsForList().range(RedisKeyConstants.website_data_list, offset, offset + 100l);
+		} catch (Exception e) {
+			LOG.warn("WebsiteRedisDao.getRollListDataByOffset throw exception : " + e.getMessage());
+		}
+		if (list == null) {
+			list = new ArrayList<>();
+		}
+		return list;
 	}
 
 }
